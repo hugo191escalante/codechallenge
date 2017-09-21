c7bf46b0d86c9413a01fe0ecdc300ab87c67d0b0
Mona Lisa <mona@lisa.com>
2017-09-20 22:14:25 -0400

    Added hello world

--- Umbrella/src/main/java/com/foo/umbrella/ui/MainActivity.java
+++ Umbrella/src/main/java/com/foo/umbrella/ui/MainActivity.java
@@ -2,14 +2,47 @@ package com.foo.umbrella.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.foo.umbrella.R;
+import com.foo.umbrella.data.ApiServicesProvider;
+import com.foo.umbrella.data.model.WeatherData;
+
+import retrofit2.adapter.rxjava.Result;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ApiServicesProvider apiServicesProvider = new ApiServicesProvider(getApplication());
+        apiServicesProvider.getWeatherService().forecastForZipObservable("30339")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Result<WeatherData>>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d(TAG, "onCompleted: ");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "onError: ");
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onNext(Result<WeatherData> weatherDataResult) {
+                        Log.d(TAG, "onNext: " + weatherDataResult.response().body());
+                    }
+                });
     }
 }
